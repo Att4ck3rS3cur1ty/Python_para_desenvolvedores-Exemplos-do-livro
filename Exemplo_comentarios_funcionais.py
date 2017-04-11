e8fe2d9d2bd3712418573284f933cb13bae1a163
Att4ck3rS3cur1ty <abimaelima54@gmail.com>
2017-04-11 19:47:01 -0300

    Revert "Exemplo de comentários funcionais"
    This reverts commit bf68808eab26c3fedec1c4a8b99bb317c8a1902b.

--- Exemplo_comentarios_funcionais.py
+++
@@ -1,9 +0,0 @@
-#!usr/bin/env python
-# Uma linha de código que mostra o resultado de 7 vezes 3
-print(7*3)
-
-# Para continuar o comando em outra linha, a barra invertida poderia ter sido adicionada:
-print(7
-    \
-      * 3)
-
