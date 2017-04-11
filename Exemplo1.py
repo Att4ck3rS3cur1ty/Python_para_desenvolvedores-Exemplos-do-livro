488cb574fdde0ebea51fb70942b4683aefc59666
Att4ck3rS3cur1ty <abimaelima54@gmail.com>
2017-04-11 18:10:02 -0300

    Exemplo 1
    Lista de instrumentos

---
+++ Exemplo1.py
@@ -0,0 +1,6 @@
+# Uma lista de instrumentos musicais
+instrumentos = ['Baixo', 'Bateria', 'Guitarra']
+
+for instrumento in instrumentos:
+    # mostra o nome do instrumento musical
+    print(instrumento)
