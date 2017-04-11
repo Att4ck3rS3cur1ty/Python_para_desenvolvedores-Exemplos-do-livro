f2161a55d6df9e5a694d2293de088b7497cbbc39
Att4ck3rS3cur1ty <abimaelima54@gmail.com>
2017-04-11 20:02:11 -0300

    Linhas e lista quebradas por vírgula

---
+++ Exemplo_linhas_quebradas.py
@@ -0,0 +1,4 @@
+# Uma linha quebrada por contra-barra
+a = 7 * 3 + \
+    5 / 2
+print(a)
---
+++ Exemplo_lista(quebrada_por_vírgula).py
@@ -0,0 +1,6 @@
+# Uma lista (quebrada por vírgula)
+b = ['a', 'b', 'c',
+     'd', 'e']
+
+for b_letter in b:
+    print(b_letter)
