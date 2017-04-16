e7cccb51bd81382d2ff9c410f9a8209936933e46
Att4ck3rS3cur1ty <abimaelima54@gmail.com>
2017-04-16 01:59:14 -0300

    Funções bultin para tipos numéricos
    Valor absoluto de um número. conversão de decimal para octal. conversão
    de decimal para hexadecimal, elevar um número por outro, retorno de um
    número real com arredondamento especificado

---
+++ Funções_bultin_operadores.py
@@ -0,0 +1,16 @@
+# Valor absoluto de um número
+print(abs(-798))
+
+# Conversão de decimal para octal
+print(oct(20))
+
+# Conversão de decimal para hexadecimal
+print(hex(20))
+
+# Elevar um número por outro
+print(pow(2, 4))
+
+# Retorna um número real com arredondamento especificado
+print(round(3.1416, 2))
+
+
